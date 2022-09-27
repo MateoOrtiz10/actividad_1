3117a330848b4029332af624e1692d849303cb5b
Mateo <itj.17131@gmail.com>
2022-09-27 00:58:39 -0500

    Otros cambios

--- lib/screens/game_screen_1.dart
+++ lib/screens/game_screen_1.dart
@@ -1,21 +1,39 @@
 import 'package:flutter/material.dart';
 
-class GameScreen1 extends StatelessWidget {
+class GameScreen1 extends StatefulWidget {
   const GameScreen1({super.key});
 
   @override
+  State<GameScreen1> createState() => _GameScreen1State();
+}
+
+
+class _GameScreen1State extends State<GameScreen1> {
+  @override
   Widget build(BuildContext context) {
     
-    return Scaffold(     
-      body: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          image: const DecorationImage(
-            image: AssetImage('assets/image-4.png'),
-            fit: BoxFit.cover
-          ) 
-        ),
-      )      
-    );    
+    return Scaffold(         
+        body: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                  decoration: BoxDecoration(
+                  color: Colors.white,
+                  image: DecorationImage(
+                  image: AssetImage('assets/image-4.png'),
+                  fit: BoxFit.cover,                 
+            ), 
+          ), 
+          child: Center(             
+              child: Row(
+                children: [                 
+                Image.asset('assets/image-7.png', color: Colors.white),
+                Image.asset('assets/image-7.png', color: Colors.white),
+                Image.asset('assets/image-7.png', color: Colors.white),
+                Image.asset('assets/image-7.png', color: Colors.white),
+                Image.asset('assets/image-7.png'),                                            
+                ],                
+              ),
+            ), 
+          )          
+        ); 
   }
 }
